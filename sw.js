0df821a7f54adf405ab319a7d60cfe5d1977a78b
jonohrberg <jonohrberg@gmail.com>
2026-06-17 22:52:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="rosesol-cache-v1";
+const C="rosesol-cache-v2";
 const ASSETS=["/","/index.html","/manifest.webmanifest"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).catch(()=>{}).then(()=>self.skipWaiting()));
